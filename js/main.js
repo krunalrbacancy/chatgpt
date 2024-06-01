d5c7559307fa061865e1f946511b349738344220
Krunal <157491610+krunalrbacancy@users.noreply.github.com>
2024-06-01 19:27:17 +0530

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+    // SideBar Toggle JS
+    $('#toggle-sidebar').click(function() {
+        $(this).hide();
+        $('aside').css('width', '0px').css('visibility', 'hidden');
+        $('#show-toggle').removeClass('hidden')
+    })
+    $('#show-toggle-btn').click(function() {
+        $('aside').css('width', '260px').css('visibility', 'visible');
+        $('#toggle-sidebar').show();
+        $('#show-toggle').addClass('hidden')
+    })
+    // SideBar Toggle JS
+
+    // New Chat JS
+    $('.add-new-chat').click(function() {
+       $('#chat-body').addClass('hidden') 
+       $('#inial-part-new').removeClass('hidden');
+    });
+    // New Chat JS
+})
