ec57f5c0a3e195e0600c2a7036637b0dd229da09
Krunal <157491610+krunalrbacancy@users.noreply.github.com>
2024-06-02 20:26:26 +0530

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -3,12 +3,12 @@ $(document).ready(function() {
     $('#toggle-sidebar').click(function() {
         $(this).hide();
         $('aside').css('width', '0px').css('visibility', 'hidden');
-        $('#show-toggle').removeClass('hidden')
+        $('#show-toggle').removeClass('lg:hidden')
     })
     $('#show-toggle-btn').click(function() {
         $('aside').css('width', '260px').css('visibility', 'visible');
         $('#toggle-sidebar').show();
-        $('#show-toggle').addClass('hidden')
+        $('#show-toggle').addClass('lg:hidden')
     })
     // SideBar Toggle JS
 
